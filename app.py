fc7cc4d1545b0ede5823326a3e5ab1c122c3a655
nutcasericky <rtan20317@gmail.com>
2022-10-13 13:04:23 +0800

    ni

---
+++ app.py
@@ -0,0 +1,25 @@
+import random
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/no', methods = ["POST", "GET"])
+def no():
+  output = request.form.to_dict()
+  name = output["name"]
+  msg = gay_calculator(name)
+  return render_template("result.html", msg)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
+
+def gay_calculator(name):
+      i = random.randint(0, 100)
+      if i > 50: return name + " you are " + str(i) +"%" + " gay, stop lying"
+      else: return name + " you are " + str(i) +"%" + " gay, amen"
